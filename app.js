957fc88543c39e09589078c91ac5e1b9ac5992bb
Fazal Shah <bilalfazal52@gmail.com>
2025-04-07 20:20:30 +0500

    frontend origin register in backend cors policy

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser())
 app.use(express.json());
 app.use(cors({
-  origin: "*",
+  origin: "http://localhost:5173",
   credentials: true
 }));
 app.use("/api/v1", userRoutes);
