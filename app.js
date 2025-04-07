4e74993d44571ac413c65ae20af956b07d66fdbf
Fazal Shah <bilalfazal52@gmail.com>
2025-04-07 20:14:00 +0500

    origin issue handle

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser())
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use("/api/v1", userRoutes);
 
 app.get("/", (req, res) => {
