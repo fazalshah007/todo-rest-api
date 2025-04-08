2570d07610e028c338c79a30642f77c826e4440a
Fazal Shah <bilalfazal52@gmail.com>
2025-04-09 00:17:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser())
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://mern-todo-app-005.netlify.app/"],
   credentials: true
 }));
 app.use("/api/v1", userRoutes);
